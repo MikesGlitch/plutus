3589f8e05a8049fb67cc34d1b3cc41d9f909b13f
Michael Clark <mclarkgb@gmail.com>
2022-10-18 17:09:18 +0100

    routing

--- google-drive-example/plutus/src/App.tsx
+++ google-drive-example/plutus/src/App.tsx
@@ -1,7 +1,7 @@
 // import UserRegistrationForm from '@/features/users/UserRegistrationForm'
 // import UserList from '@/features/users/UserList'
-import Accounts from '@/features/accounts/Accounts'
 import TheNav from '@/components/Nav/TheNav'
+import { Outlet } from 'react-router-dom'
 
 function App () {
   return (
@@ -12,7 +12,7 @@ function App () {
         {/* Router this eventually */}
         <main className='w-full ml-60 py-4 px-3'>
           <h1 className='text-xl text-red-500'>Plan is to base most of this off of <a href="https://flowbite.com">https://flowbite.com</a> for basic components</h1>
-          <Accounts />
+          <Outlet />
         </main>
       </div>
     </div>
--- google-drive-example/plutus/src/main.tsx
+++ google-drive-example/plutus/src/main.tsx
@@ -2,9 +2,39 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import Accounts from './features/accounts/Accounts'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        path: '',
+        element: <h1>Home page - perhaps it goes to the last page you were on or maybe the budget</h1>
+      },
+      {
+        path: 'budget',
+        element: <h1>Budget</h1>
+      },
+      {
+        path: 'reports',
+        element: <h1>Reports</h1>
+      },
+      {
+        path: 'accounts',
+        element: <Accounts />
+      }
+    ]
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    {/* <BrowserRouter>
+      <App />
+    </BrowserRouter> */}
+    <RouterProvider router={router} />
   </React.StrictMode>
 )
