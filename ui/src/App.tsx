dc608be42018618e4b3c77e8dd282c558b768363
Michael Clark <mclarkgb@gmail.com>
2022-10-29 10:39:19 +0100

    an idea

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -12,6 +12,7 @@ function App () {
         {/* Router this eventually */}
         <main className='w-full ml-60 py-4 px-3'>
           <h1 className='text-xl text-red-500'>Plan is to base most of this off of <a href="https://flowbite.com">https://flowbite.com</a> for basic components</h1>
+          <h1 className='text-xl text-pink-500'>Make a `Features to build` page so I don`t have to write this in the readme then never read it. And add a budget number checker - you could scan the internet for the best deals, then compare what the person is payign for energy/broadband/mobile etc. And tell them they`re paying a lot or a little for the thing.</h1>
           <Outlet />
         </main>
       </div>
