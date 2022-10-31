5da7ba0f832aee365f7a131bb993f8dc42d893ea
Michael Clark <mclarkgb@gmail.com>
2022-10-31 17:43:59 +0000

    lil bittssS

--- ui/src/components/Nav/TheNav.tsx
+++ ui/src/components/Nav/TheNav.tsx
@@ -3,40 +3,97 @@ import { Link } from 'react-router-dom'
 export default function TheNav () {
   return (
     <aside className="fixed top-0 bottom-0 h-full w-60" aria-label="Sidebar">
-        <div className="overflow-y-auto h-full py-4 px-3 bg-gray-50 dark:bg-gray-800">
-            <Link to={'/'} className="flex items-center pl-2.5 mb-5">
-                {/* <img src="https://flowbite.com/docs/images/logo.svg" className="mr-3 h-6 sm:h-7" alt="Flowbite Logo" /> */}
-                <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Plutus</span>
+      <div className="overflow-y-auto h-full py-4 px-3 bg-gray-50 dark:bg-gray-800">
+        <Link to={'/'} className="flex items-center pl-2.5 mb-5">
+          {/* <img src="https://flowbite.com/docs/images/logo.svg" className="mr-3 h-6 sm:h-7" alt="Flowbite Logo" /> */}
+          <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
+            Plutus
+          </span>
+        </Link>
+        <ul className="space-y-2">
+          <li>
+            <Link
+              className="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700"
+              to={'budget'}
+            >
+              <svg
+                aria-hidden="true"
+                className="w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path d="M2 10a8 8 0 018-8v8h8a8 8 0 11-16 0z"></path>
+                <path d="M12 2.252A8.014 8.014 0 0117.748 8H12V2.252z"></path>
+              </svg>
+              <span className="ml-3">Budget</span>
             </Link>
-            <ul className="space-y-2">
-                <li>
-                    <Link className="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700" to={'budget'}>
-                        <svg aria-hidden="true" className="w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M2 10a8 8 0 018-8v8h8a8 8 0 11-16 0z"></path><path d="M12 2.252A8.014 8.014 0 0117.748 8H12V2.252z"></path></svg>
-                        <span className="ml-3">Budget</span>
-                    </Link>
-                </li>
-                <li>
-                    <Link className="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700" to={'reports'}>
-                        <svg aria-hidden="true" className="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M5 3a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2V5a2 2 0 00-2-2H5zM5 11a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2v-2a2 2 0 00-2-2H5zM11 5a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V5zM11 13a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2v-2z"></path></svg>
-                        <span className="flex-1 ml-3 whitespace-nowrap">Reports</span>
-                        <span className="inline-flex justify-center items-center px-2 ml-3 text-sm font-medium text-gray-800 bg-gray-200 rounded-full dark:bg-gray-700 dark:text-gray-300">Pro</span>
-                    </Link>
-                </li>
-                <li>
-                    <Link className="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700" to={'accounts'}>
-                        <svg aria-hidden="true" className="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M8.707 7.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l2-2a1 1 0 00-1.414-1.414L11 7.586V3a1 1 0 10-2 0v4.586l-.293-.293z"></path><path d="M3 5a2 2 0 012-2h1a1 1 0 010 2H5v7h2l1 2h4l1-2h2V5h-1a1 1 0 110-2h1a2 2 0 012 2v10a2 2 0 01-2 2H5a2 2 0 01-2-2V5z"></path></svg>
-                        <span className="flex-1 ml-3 whitespace-nowrap">All Accounts</span>
-                        <span className="inline-flex justify-center items-center p-3 ml-3 w-3 h-3 text-sm font-medium text-blue-600 bg-blue-200 rounded-full dark:bg-blue-900 dark:text-blue-200">3</span>
-                    </Link>
-                </li>
-                <li>
-                    <Link className="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700" to={'upcoming-features'}>
-                        <svg aria-hidden="true" className="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M8.707 7.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l2-2a1 1 0 00-1.414-1.414L11 7.586V3a1 1 0 10-2 0v4.586l-.293-.293z"></path><path d="M3 5a2 2 0 012-2h1a1 1 0 010 2H5v7h2l1 2h4l1-2h2V5h-1a1 1 0 110-2h1a2 2 0 012 2v10a2 2 0 01-2 2H5a2 2 0 01-2-2V5z"></path></svg>
-                        <span className="flex-1 ml-3 whitespace-nowrap">Upcoming features</span>
-                    </Link>
-                </li>
-            </ul>
-        </div>
+          </li>
+          <li>
+            <Link
+              className="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700"
+              to={'reports'}
+            >
+              <svg
+                aria-hidden="true"
+                className="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path d="M5 3a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2V5a2 2 0 00-2-2H5zM5 11a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2v-2a2 2 0 00-2-2H5zM11 5a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V5zM11 13a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2v-2z"></path>
+              </svg>
+              <span className="flex-1 ml-3 whitespace-nowrap">Reports</span>
+              <span className="inline-flex justify-center items-center px-2 ml-3 text-sm font-medium text-gray-800 bg-gray-200 rounded-full dark:bg-gray-700 dark:text-gray-300">
+                Pro
+              </span>
+            </Link>
+          </li>
+          <li>
+            <Link
+              className="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700"
+              to={'accounts'}
+            >
+              <svg
+                aria-hidden="true"
+                className="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path d="M8.707 7.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l2-2a1 1 0 00-1.414-1.414L11 7.586V3a1 1 0 10-2 0v4.586l-.293-.293z"></path>
+                <path d="M3 5a2 2 0 012-2h1a1 1 0 010 2H5v7h2l1 2h4l1-2h2V5h-1a1 1 0 110-2h1a2 2 0 012 2v10a2 2 0 01-2 2H5a2 2 0 01-2-2V5z"></path>
+              </svg>
+              <span className="flex-1 ml-3 whitespace-nowrap">
+                All Accounts
+              </span>
+              <span className="inline-flex justify-center items-center p-3 ml-3 w-3 h-3 text-sm font-medium text-blue-600 bg-blue-200 rounded-full dark:bg-blue-900 dark:text-blue-200">
+                3
+              </span>
+            </Link>
+          </li>
+          <li>
+            <Link
+              className="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700"
+              to={'upcoming-features'}
+            >
+              <svg
+                aria-hidden="true"
+                className="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path d="M8.707 7.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l2-2a1 1 0 00-1.414-1.414L11 7.586V3a1 1 0 10-2 0v4.586l-.293-.293z"></path>
+                <path d="M3 5a2 2 0 012-2h1a1 1 0 010 2H5v7h2l1 2h4l1-2h2V5h-1a1 1 0 110-2h1a2 2 0 012 2v10a2 2 0 01-2 2H5a2 2 0 01-2-2V5z"></path>
+              </svg>
+              <span className="flex-1 ml-3 whitespace-nowrap">
+                Upcoming features
+              </span>
+            </Link>
+          </li>
+        </ul>
+      </div>
     </aside>
   )
 }
--- ui/src/features/budget/index.tsx
+++ ui/src/features/budget/index.tsx
@@ -1,5 +1,6 @@
 import InputCurrency from '@/components/Form/InputCurrency'
 import { useEffect, useState } from 'react'
+import { db } from '../../db'
 
 function HeaderCell ({ name }: { name: string }) {
   return (<div className="outline outline-1 outline-gray-300 px-2 font-bold">{ name }</div>)
@@ -58,16 +59,55 @@ function Budget () {
   const [rows, setRows] = useState<any[]>([])
 
   useEffect(() => {
-    setCategoryColumns([{ key: 'category', name: 'Category' }])
-    setCategoryRows([{ category: 'Category 1' }, { category: 'Category 2' }, { category: 'Category 3' }])
+    // setCategoryColumns([{ key: 'category', name: 'Category' }])
+    // setCategoryRows([{ category: 'Category 1' }, { category: 'Category 2' }, { category: 'Category 3' }])
 
-    setColumns([{ key: 'budgeted', name: 'Budgeted' }, { key: 'outflows', name: 'Outflows' }, { key: 'balance', name: 'Balance' }])
-    setRows([{ budgeted: 1.22, outflows: 9, balance: -1.25 }, { budgeted: 58, outflows: 33.25, balance: -98.25 }, { budgeted: 5522.25, outflows: 12.32, balance: 21.25 }])
+    // setColumns([{ key: 'budgeted', name: 'Budgeted' }, { key: 'outflows', name: 'Outflows' }, { key: 'balance', name: 'Balance' }])
+    // setRows([{ budgeted: 1.22, outflows: 9, balance: -1.25 }, { budgeted: 58, outflows: 33.25, balance: -98.25 }, { budgeted: 5522.25, outflows: 12.32, balance: 21.25 }])
+  }, [])
+
+  useEffect(() => {
+    async function getBudget () {
+      try {
+        setCategoryColumns([{ key: 'category', name: 'Category' }])
+        const dbCategories = await db.categories.toArray()
+        const allCategories = dbCategories.map((category) => {
+          return { category: category.name }
+        })
+        setCategoryRows(allCategories)
+
+        // For this month
+        setColumns([{ key: 'budgeted', name: 'Budgeted' }, { key: 'outflows', name: 'Outflows' }, { key: 'balance', name: 'Balance' }])
+
+        const dbTransactions = await db.transactions.toArray()
+        const transactionsThisMonth = dbCategories.map((category) => {
+          const transactionsForCategory = dbTransactions.filter((transaction) => transaction.categoryId === category.id)
+          const sumOfTransactionsPennies = transactionsForCategory.reduce((prevValue, currentValue) => {
+            return prevValue + currentValue.amountPennies
+          }, 0)
+
+          const budgeted: number | undefined = undefined
+
+          return {
+            budgeted,
+            outflows: sumOfTransactionsPennies,
+            balance: budgeted === undefined ? 0 - sumOfTransactionsPennies : budgeted - sumOfTransactionsPennies
+          }
+        })
+
+        setRows(transactionsThisMonth)
+      } catch (error) {
+        console.error('something bad happened', error)
+      }
+    }
+
+    getBudget()
   }, [])
 
   return (
       <>
         <h1>Budget - similar to <a href="https://github.com/adazzle/react-data-grid">https://github.com/adazzle/react-data-grid</a></h1>
+        <pre>Categories: { JSON.stringify(categoryRows) }</pre>
         <div className='flex gap-4'>
           <div className='w-64'>
             <DataGrid rows={categoryRows} columns={categoryColumns} onRowsChange={(newRows) => { return newRows } }></DataGrid>
