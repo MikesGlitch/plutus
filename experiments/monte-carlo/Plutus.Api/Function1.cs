771deb49c2c378da0a663c722b5fce22bbdcfb37
Mike Clark <mclarkgb@gmail.com>
2023-08-20 22:55:29 +0100

    bits and bobs

--- experiments/monte-carlo/Plutus.Api/Function1.cs
+++ experiments/monte-carlo/Plutus.Api/Function1.cs
@@ -1,8 +1,6 @@
 using System.Net;
-using System.Threading.Tasks;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Http;
-using Microsoft.Extensions.Logging;
 
 using Plutus.Application;
 
@@ -10,13 +8,6 @@ namespace Plutus.Api
 {
     public class Function1
     {
-        private readonly ILogger _logger;
-
-        public Function1(ILoggerFactory loggerFactory)
-        {
-            _logger = loggerFactory.CreateLogger<Function1>();
-        }
-
         [Function("Function1")]
         public async Task<HttpResponseData> Run([HttpTrigger(AuthorizationLevel.Function, "get")] HttpRequestData req)
         {
