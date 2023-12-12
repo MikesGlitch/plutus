15b4ab7e8b99a9238ca6d40e2ae8f59e8f6b751c
Mike Clark <mclarkgb@gmail.com>
2023-12-12 22:52:43 +0000

    lets see

--- experiments/plutus-go/api/index.go
+++ experiments/plutus-go/api/index.go
@@ -1,13 +1,13 @@
-// /api/index.go
 package handler
 
 import (
-	"fmt"
 	"net/http"
+	"time"
+
+	"github.com/mikesglitch/plutus/components"
 )
 
-// https://plutus-mikesglitch.vercel.app/index
-// Oh look, an example: https://github.com/riccardogiorato/template-go-vercel
-func HelloWorld(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello World from Go!")
+func Handler(w http.ResponseWriter, r *http.Request) {
+	formattedTime := time.Now().Format(time.RFC850)
+	components.Page(formattedTime).Render(r.Context(), w)
 }
---
+++ experiments/plutus-go/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"net/http"
+	"time"
+	"fmt"
+
+	"github.com/mikesglitch/plutus/components"
+)
+
+func main() {
+	// Test harness - Vercel only reads the api directory so this should ideally point to that after spinning up
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		formattedTime := time.Now().Format(time.RFC850)
+		components.Page(formattedTime).Render(r.Context(), w)
+	})
+
+	fmt.Println("Listening on 127.0.0.1:4000")
+	http.ListenAndServe("127.0.0.1:4000", nil)
+}
