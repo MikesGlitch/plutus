d632bb0a6c7291cad595fcd04b3400c7ac5c4841
Mike Clark <mclarkgb@gmail.com>
2023-12-13 23:42:00 +0000

    unique names even if in different dir?

--- experiments/plutus-go/api/notempl.go
+++ experiments/plutus-go/api/notempl.go
@@ -14,7 +14,7 @@ type NoTemplViewModel struct {
 func NoTemplHandler(w http.ResponseWriter, r *http.Request) {
 	var tmplt *template.Template
 	formattedTime := time.Now().Format(time.RFC850)
-	tmplt, _ = template.ParseFiles("notempl.html")
+	tmplt, _ = template.ParseFiles("notempltest.html")
 	vm := NoTemplViewModel{
 		Time: formattedTime,
 	}
