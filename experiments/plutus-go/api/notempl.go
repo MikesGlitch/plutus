a8a9d66b2700b6ef05f21e88ea1775e805a4b439
Mike Clark <mclarkgb@gmail.com>
2023-12-13 23:59:15 +0000

    come on

--- experiments/plutus-go/api/notempl.go
+++ experiments/plutus-go/api/notempl.go
@@ -3,23 +3,26 @@ package handler
 import (
 	"html/template"
 	"net/http"
-	"time"
+	// "time"
 )
 
 type NoTemplViewModel struct {
 	Time string
 }
 
+var tmplt *template.Template
+
 // https://www.makeuseof.com/go-html-templating/
 func NoTemplHandler(w http.ResponseWriter, r *http.Request) {
-	var tmplt *template.Template
-	formattedTime := time.Now().Format(time.RFC850)
-	tmplt, _ = template.ParseFiles("public/notempltest.html") // throwing them in public to see if it picks them up
-	vm := NoTemplViewModel{
-		Time: formattedTime,
-	}
+	// formattedTime := time.Now().Format(time.RFC850)
+	tmplt, _ = template.ParseFiles("notempltest.html") // throwing them in public to see if it picks them up
+	// vm := NoTemplViewModel{
+	// 	Time: formattedTime,
+	// }
 
-	err := tmplt.Execute(w, vm)
+	println("are we here?")
+
+	err := tmplt.Execute(w, nil)
 
 	if err != nil {
 		return
