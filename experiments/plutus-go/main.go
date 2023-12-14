403697760d3d29510de057e695e69395bb8776d1
Mike Clark <mclarkgb@gmail.com>
2023-12-14 00:40:00 +0000

    that's a shame

--- experiments/plutus-go/main.go
+++ experiments/plutus-go/main.go
@@ -2,44 +2,20 @@ package main
 
 import (
 	"fmt"
-	"html/template"
 	"net/http"
-	"os"
 	"time"
+
+	"github.com/mikesglitch/plutus/components/pages/index"
 )
 
-type NoTemplViewModel struct {
-	Time string
-}
-
-var tmplt *template.Template
-
 func main() {
-	mydir, _ := os.Getwd()
-	entries, _ := os.ReadDir(mydir)
-	for _, e := range entries {
-		fmt.Println(e.Name())
-	}
-
-	println("are we here?", mydir)
 	// Test harness - Vercel only reads the api directory so this should ideally point to that after spinning up
 	// This is wrong, it seems to be taking in all requests at this - need some work to make it fit with the vercel config
-
+	// FYI: we're using templ cause vercel/other deployments wont always recognise a .html file as a part of deploy - they need a .go file.
+	// Templ generates the html files as .go files which allows them to be picked up by Serverless deploys
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		formattedTime := time.Now().Format(time.RFC850)
-
-		tmplt, _ = template.ParseFiles("./components/pages/notempl/notempl.html")
-
-		vm := NoTemplViewModel{
-			Time: formattedTime,
-		}
-
-		println("are we here?", tmplt)
-		err := tmplt.Execute(w, vm)
-
-		if err != nil {
-			return
-		}
+		index.Page(formattedTime).Render(r.Context(), w)
 	})
 
 	fmt.Println("Listening on 127.0.0.1:4000")
